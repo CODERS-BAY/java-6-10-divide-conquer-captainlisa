b2372385616b910b20b1d6fd6a9b7efc2f8dafd1
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-03-02 14:52:30 +0100

    Add Divide and Conquer Algorithm

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -3,7 +3,33 @@ package com.codersbay;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Divide and conquer");
+        System.out.println("Divide and conquer!");
+
+        int[] array = {7, 45, -234, 876, 46, 0, -2, 17};
+        System.out.println(findMaximum(array));
+
     }
 
+    private static int findMaximum(int[] array) {
+        if (array.length > 2) {
+            int middle = array.length / 2;
+            int[] leftSide = new int[middle];
+            int[] rightSide = new int[array.length - middle];
+            for (int i = 0; i < middle; i++) {
+                leftSide[i] = array[i];
+            }
+            int left = findMaximum(leftSide);
+
+            for (int i = middle; i < array.length; i++) {
+                rightSide[i - middle] = array[i];
+            }
+            int right = findMaximum(rightSide);
+            return left > right ? left : right;
+        } else if (array.length == 2) {
+            return array[0] > array[1] ? array[0] : array[1];
+        } else {
+            return array[0];
+        }
+
+    }
 }
